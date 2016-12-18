55fce7a72696afdcf97228e20fe70ac572a88ab9
Scott Dixon <dixon.scottr@gmail.com>
2016-12-18 18:45:16 -0500

    Refactor number and operators with button component

--- src/Buttons.js
+++ src/Buttons.js
@@ -7,10 +7,14 @@ class Buttons extends Component {
   render() {
     return(
       <div className='button-holder'>
-        <NumberButtons range={[0,9]} onUpdate={this.props.onUpdate.bind(this)} />
-        <Operators btns={['+', '-', '/', '*']} onUpdate={this.props.onUpdate.bind(this)} />
-        <input type='button' className="equal-button mdl-button mdl-js-button mdl-button--raised" value='=' onClick={this.props.onResult.bind()} />
-        <input type='button' className='clear-button mdl-button mdl-js-button mdl-button--raised' value='clear' onClick={this.props.reset.bind()}/>
+        <div className='top-btns'>
+          <NumberButtons nums={[0,1,2,3,4,5,6,7,8,9]} onUpdate={this.props.onUpdate.bind(this)} />
+          <Operators btns={['+', '-', '/', '*']} onUpdate={this.props.onUpdate.bind(this)} />
+        </div>
+        <div className='btm-btns'>
+          <input type='button' className="equal-button mdl-button mdl-js-button mdl-button--raised" value='=' onClick={this.props.onResult.bind()} />
+          <input type='button' className='clear-button mdl-button mdl-js-button mdl-button--raised' value='clear' onClick={this.props.reset.bind()}/>
+        </div>
       </div>
     )
   }
--- src/Operators.js
+++ src/Operators.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from './Button'
 
 class Operators extends Component {
   render() {
     const btns = this.props.btns
     return(
-      <div>
+      <div className='operators'>
         {btns.map(btn => {
           return <Button
             btn={btn}
             handleClick={this.props.onUpdate.bind(btn)}
             classes='mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab op-button'
+            key={btn}
             />
         })}
       </div>
@@ -18,17 +20,4 @@ class Operators extends Component {
   }
 }
 
-const Button = React.createClass({
-  render() {
-    return (
-      <input type='button' value={this.props.btn} className={this.props.classes} onClick={this.props.handleClick} />
-    )
-  }
-})
-
 export default Operators
-
-// <input type='button'  value='+' className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab op-button" onClick={this.props.onUpdate.bind('+')} />
-// <input type='button' value='-' className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab op-button" onClick={this.props.onUpdate.bind('-')} />
-// <input type='button' value='*' className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab op-button" onClick={this.props.onUpdate.bind('*')} />
-// <input type='button' value='/' className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab op-button" onClick={this.props.onUpdate.bind('/')} />
